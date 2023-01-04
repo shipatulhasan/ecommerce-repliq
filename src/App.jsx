8f40ea677ed67c0ff6fe3f408ae40c30112bd269
shipatulhasan <shipatulhasan328@gmail.com>
2023-01-04 20:44:12 +0600

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+
+  return (
+    <div>
+      <h2 className="text-center text-5xl text-slate-700">
+        Hello repliq
+      </h2>
+   
+    </div>
+  )
+}
+
+export default App
